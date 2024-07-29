90b21f64f63269bacba8dcc3273c0f82768cb33c
guidev <aimenguidad@gmail.com>
2024-07-29 19:47:16 +0100

    feat: create app screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-function App() {
+import React from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Column from "src/components/Column";
+import useUsers from "./store/users";
+
+const App: React.FC = () => {
+  const { short, interview, applied } = useUsers((store) => store);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn btn-primary">Button</button>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="flex flex-col lg:flex-row gap-8 p-5">
+        <Column users={applied} listId="applied" />
+        <Column users={short} listId="short" />
+        <Column users={interview} listId="interview" />
+      </div>
+    </DndProvider>
   );
-}
+};
 
 export default App;
